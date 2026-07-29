5d33996bb79ad370f9fa44b0a165543356317810
shobhit0296 <shobhit0296@github.com>
2026-07-29 16:13:03 +0530

    page_link_import_fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect, useMemo } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import { STREAMING_SITES, SiteItem } from "./data";
 
